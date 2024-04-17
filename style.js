fe6dd8a182e8facbb507b6925ef56c50349f72be
EliMarago <156212818+EliMarago@users.noreply.github.com>
2024-04-17 22:32:52 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,44 @@
+const btn = document.querySelector(".button");
+const input = document.getElementById("input");
+const list = document.getElementById("list-container");
+
+const addTask = function () {
+  if (input.value === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  input.value = "";
+  saveData();
+};
+btn.addEventListener("click", addTask);
+
+list.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") e.target.parentElement.remove();
+    saveData();
+  },
+  false
+);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") addTask();
+});
+
+function saveData() {
+  localStorage.setItem("data", list.innerHTML);
+}
+function showTask() {
+  list.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
